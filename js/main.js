e19a64da2c4e9bd50998f6946aa93f9312d544d2
nbrender <67350372+nbrender@users.noreply.github.com>
2020-06-24 09:50:22 -0700

    Updated clock formatting

--- js/main.js
+++ js/main.js
@@ -30,10 +30,10 @@ function getDateTime() {
         hour = 24 + hour;
     }
 
-    let date = (day < 10 ? '0' + day : day) + '/' + (month < 10 ? '0' + month : month) + '/' + dateTime.getFullYear();
-    let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+    let date = (month < 10 ? '0' + month : month) + '/' + (day < 10 ? '0' + day : day) + '/' + dateTime.getFullYear();
+    let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) /*+ ':' + (seconds < 10 ? '0' + seconds : seconds)*/ ;
 
-    return date + '\n' + time;
+    return date + ' ' + time;
 }
 
 function setClock() {
